c7c725c2ac8c2c6dfaf282a651e8df3391c9a8f1
Shri <ssthakar@ncsu.edu>
2024-09-02 00:03:11 -0400

    pushing code

--- qs.py
+++ qs.py
@@ -1,14 +1,42 @@
+import timeit
+import time
+
 import jax.numpy as jnp
-from jax import random
+from jax import block_until_ready, random
 from jax import jit
+from jax import grad
 
+from jax.lib import xla_bridge
 
-# simple activation function
+# simple activation function for jit benchmarking
 def selu(x, alpha=1.67, lmbda=1.05):
     alpha = jnp.array(alpha)
     lmbda = jnp.array(lmbda)
     return lmbda * jnp.where(x > 0, x, alpha * jnp.exp(x) - alpha)
 
-x = jnp.arange(5.0)
-print(selu(x))
+
+
+if __name__ == '__main__':  
+    
+    # check if nvidia is recognized
+    print(xla_bridge.get_backend().platform)
+
+    # generate large array of random numbers
+    key = random.key(1701)
+    x = random.normal(key,(1_000_000,))
+    t1 = timeit.timeit(lambda: selu(x,alpha=1.67,lmbda=1.05).block_until_ready(),timer=time.perf_counter,number=1,)
+    print(x.devices())
+    # just in time compilation
+    selu_jit = jit(selu)
+
+    # compiles the function because it is called
+    _ = selu_jit(x) 
+    t2 = timeit.timeit(lambda: selu_jit(x,alpha=1.67,lmbda=1.05).block_until_ready(),timer=time.perf_counter,number=1,)
+    # display times and speed up
+    print(f'time without JIT: {t1} secs\ntime with JIT: {t2} secs')
+    
+    # taking derivatives
+
+    
+
 
